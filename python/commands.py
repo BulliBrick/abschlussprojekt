d92e459c1892974b9f0fa9e64910d20bb608c1b0
Simon Amsler <simon.amsler@bbzbl-it.ch>
2024-03-27 14:21:31 +0100

    commit

--- python/commands.py
+++ python/commands.py
@@ -1,6 +1,5 @@
-
 # Importiert den MYSQL Python Connector
-import mysql.connector
+import mysql.connector 
 
 
 # Verbindet sich mit der Datenbank 'abschlussarbeit' auf dem port 3306 mit dem User 'root' und dem Passwort 'root'. Gibt aus sobald die Verbindung hergestellt wurde.
@@ -10,22 +9,20 @@ print("Connected to abschlussarbeit")
 
 
 # Entfehrne die '''  am Anfang und Ende um den Code zu aktivieren. Dies ist ein beispiel Eintrag in die Tabelle. Dieser Eintrag verwendet den python Mysql connector.
-'''
+
 # Erstellt einen Cursor um SQL Befehle auszuführen
 cursor = connection.cursor()
 
 # SQL querry für neuen Eintrag in der Tabelle 'members'
-sql = "INSERT INTO members (vorname, nachname, spitzname, age) VALUES (%s, %s, %s, %s)"
+sql = "INSERT INTO members (nachname, vorname, spitzname, age) VALUES (%s, %s, %s, %s)"
 
 # Definiert die Variablen für die Spalten in der Tabelle 'members'
-vorname = ""
-nachname = ""
-spitzname = ""
-age = 25
+nachname = "Arlt"
+vorname = "Johnatan"
+spitzname = "Jonny"
+age = 18
 
 # Führt den SQL Befehl aus und fügt die Variablen in die Tabelle 'members' ein
 cursor.execute(sql, (vorname, nachname, spitzname, age))
 connection.commit()
 
-
-'''
